afef9da97d5a97405526c5e613d032ea373239ca
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-04-01 18:14:17 +0530

    transfer function added

--- programs/example1/client/client.js
+++ programs/example1/client/client.js
@@ -1,5 +1,7 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
+const solanaWeb3 = require('@solana/web3.js');
+
 // import { SystemProgram } from '@solana/web3.js';
 // const SystemProgram = require("@solana/web3.js")
 const { SystemProgram } = anchor.web3;
@@ -8,35 +10,20 @@ const program = anchor.workspace.Example1;
 const programId = new anchor.web3.PublicKey("4Y8PGKfY7q5hxDA17h6UHk5eACo6k9idy1chiHb7HKsp");
 // async function main (){}
 async function deposit() {
-    const userPubKey = "5wbnW6cAoT2Rdwk5ecdgpCzoCp3g9rV7Vr5jNFAJBbCY";
-    const referrerPubKey = "";
+    const userPubKey = new anchor.web3.PublicKey("5wbnW6cAoT2Rdwk5ecdgpCzoCp3g9rV7Vr5jNFAJBbCY");
+    // const referrerPubKey = new solanaWeb3.PublicKey("");
     const remainingAccounts=[];
     const idl = await anchor.Program.fetchIdl(programId, provider);
   const program = new anchor.Program(idl, programId, provider);
   let amount = 2;
-    // const instructions = [
-    //     await program.instruction.deposit(amount,{
-    //         accounts: {
-    //             fromWallet: userPubKey,
-    //             payer: userPubKey,
-    //             referrer: referrerPubKey,
-    //             systemProgram: SystemProgram.programId,
-    //             rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-    //             clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
-    //             recentBlockhashes: anchor.web3.SYSVAR_RECENT_BLOCKHASHES_PUBKEY,
-    //             instructionSysvarAccount: anchor.web3.SYSVAR_INSTRUCTIONS_PUBKEY,
-    //         },
-    //         remainingAccounts:
-    //           remainingAccounts.length > 0 ? remainingAccounts : undefined,
-    //       }),
-    //   ];
     
-    await program.rpc.deposit({
+    
+    await program.rpc.deposit(amount,{
       accounts: {
-          fromWallet: userPubKey,
-          payer: userPubKey,
-          referrer: referrerPubKey,
-          systemProgram: SystemProgram.programId,
+          pool: userPubKey,
+          vault: userPubKey,
+         // referrer: userPubKey,
+          //systemProgram: SystemProgram.programId,
           // rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           // clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
           // recentBlockhashes: anchor.web3.SYSVAR_RECENT_BLOCKHASHES_PUBKEY,
@@ -44,7 +31,7 @@ async function deposit() {
       },
       remainingAccounts:
         remainingAccounts.length > 0 ? remainingAccounts : undefined,
-    }, amount);
+    });
 }
 deposit();
 console.log('deposit function called.')
